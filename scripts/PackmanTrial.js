4ac75afc7c18505d53b7fbcecde7c68d21081728
Angelina Polyanska <any_ole@yahoo.com>
2014-11-14 03:00:43 +0200

    new file added

--- scripts/PackmanTrial.js
+++ scripts/PackmanTrial.js
@@ -5,15 +5,26 @@ window.onload = function () {
     var theCanvas = document.getElementById("the-canvas");
     var canvasCtx = theCanvas.getContext("2d");
 
+    theCanvas.style.left = "400px";
+    theCanvas.style.top = "150px";
+    theCanvas.style.position = "absolute";
+
+
     canvasCtx.lineWidth = "2";
     canvasCtx.fillStyle = "#777";
     canvasCtx.strokeStyle = "black";
 
-    canvasCtx.fillRect(0, 0, 400, 350);
-    canvasCtx.strokeRect(0, 0, 400, 350);
 
-    canvasCtx.arc(350, 300, 30, 170*Math.PI / 180, 200*Math.PI / 180, true);
-    canvasCtx.lineTo(350,300);
+
+    canvasCtx.fillRect(0, 0, 500, 300);
+
+    canvasCtx.beginPath(0,0);
+    canvasCtx.moveTo(50, 50);
+    canvasCtx.lineTo(250, 150);
+    canvasCtx.stroke();
+
+    canvasCtx.arc(300, 250, 12, 170*Math.PI / 180, 200*Math.PI / 180, true);
+    canvasCtx.lineTo(300,250);
     canvasCtx.closePath();
     canvasCtx.fillStyle = "yellow";
     canvasCtx.fill();
@@ -22,11 +33,9 @@ window.onload = function () {
 
     canvasCtx.beginPath(360, 290);
 
-    canvasCtx.arc(360, 290, 3, 0, 2*Math.PI);
-    canvasCtx.strokeStyleStyle = "black";
-    canvasCtx.stroke();
+    canvasCtx.arc(300, 245, 2, 0, 2*Math.PI);
     canvasCtx.fillStyle = "black";
-    canvasCtx.fill();
+    canvasCtx.fill(); //*
 
 
 
